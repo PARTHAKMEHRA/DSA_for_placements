153503aa70a8fb903bf024fd153d45801292def6
PARTHAKMEHRA <84925956+PARTHAKMEHRA@users.noreply.github.com>
2022-01-12 21:35:14 +0530

    commit

---
+++ even_natural.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cout<<"enter the value of  n:";
+    cin>>n;
+    for(int i=2;i<=2*n;i=i+2){
+        cout<<i<<endl;
+    }
+    return 0;
+}
